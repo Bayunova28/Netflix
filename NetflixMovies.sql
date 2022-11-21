02eda287d473f128227833a8eb625c71d3826d6a
Willibrordus Bayu <willibayu28@gmail.com>
2022-11-21 09:20:29 +0700

    First Commit

--- NetflixMovies.sql
+++ NetflixMovies.sql
@@ -140,17 +140,19 @@ where AGE_CERTIFICATION = 'TV-MA'
 	  and RELEASE_YEAR between 2010 and 2015
 order by MOVIE_ID
 
--- Looking film that season is 4 and type is show
+-- Looking film that season is 4 and type is show order by movie ID
 select *
 from Entertaintment.dbo.Titles
 where SEASON = 4
       and TYPE = 'SHOW'
+order by MOVIE_ID
 
--- Looking film that genre is animation and production country at france
+-- Looking film that genre is animation and production country at france order by movie ID
 select *
 from Entertaintment.dbo.Titles
 where GENRE = 'Animation'
       and PRODUCTION_COUNTRY = 'FR'
+order by MOVIE_ID
 
 -- Looking imdb score by genre and actor that type is movie and production country at egypt order by imdb score descending
 select t.GENRE,
@@ -160,41 +162,28 @@ from Entertaintment.dbo.Titles t
 	 inner join Entertaintment.dbo.Credits c on t.MOVIE_ID = c.MOVIE_ID
 order by IMDB_SCORE desc
 
--- Load and import netflix titles table to Power BI Desktop
+-- Looking film that genre is comedy and production country at canada
 select *
-from 
-(
-	select row_number() over (partition by MOVIE_ID order by MOVIE_ID asc) as row_num,
-		   MOVIE_ID,
-		   TITLE,
-		   TYPE,
-		   try_convert(int, RELEASE_YEAR) as RELEASE_YEAR,
-		   AGE_CERTIFICATION,
-		   try_convert(int, RUNTIME) as RUNTIME,
-		   GENRE,
-		   PRODUCTION_COUNTRY,
-		   try_convert(int, SEASON) as SEASON,
-		   IMDB_ID,
-		   try_convert(float, IMDB_SCORE) as IMDB_SCORE,
-		   try_convert(float, IMDB_VOTES) as IMDB_VOTES,
-		   try_convert(int, TMDB_POPULARITY) as TMDB_POPULARITY,
-		   try_convert(float, TMDB_SCORE) as TMDB_SCORE
-	from Entertaintment.dbo.Titles
-	where RELEASE_YEAR between 2010 and 2020
-	      and MOVIE_ID is not null
-) as x
-where row_num = 1
+from Entertaintment.dbo.Titles
+where GENRE = 'Comedy'
+      and PRODUCTION_COUNTRY = 'CA'
 
--- Load and import netflix credits table to Power BI Desktop
+-- Looking film that age certification is TV-14 on release 2010 until 2020 order by movie ID
 select *
-from 
-(
-	select row_number() over (partition by PERSON_ID order by PERSON_ID asc) as row_num,
-		   PERSON_ID,
-		   MOVIE_ID,
-		   NAME,
-		   ROLE
-	from Entertaintment.dbo.Credits
-	where ROLE <> '#NAME?'
-) as x
-where row_num = 1
+from Entertaintment.dbo.Titles
+where AGE_CERTIFICATION = 'TV-14'
+	  and RELEASE_YEAR between 2010 and 2020
+order by MOVIE_ID
+
+-- Looking film that genre is scifi and type is movie order by movie ID
+select *
+from Entertaintment.dbo.Titles
+where GENRE = 'Scifi'
+      and TYPE = 'MOVIE'
+order by MOVIE_ID
+
+-- Looking film that genre is horror and production country at britania raya
+select *
+from Entertaintment.dbo.Titles
+where GENRE = 'Horror'
+	  and PRODUCTION_COUNTRY = 'GB'
